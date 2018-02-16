6b3a694427e9762f603e6c600e3fbe70663a1844
Tony Tung <ttung@chanzuckerberg.com>
2018-02-16 00:12:35 -0800

    add missing source files

---
+++ nbencdec/commands.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+
+from nbconvert import EncodedPythonExporter
+
+
+STRIPPED_NOTEBOOK_MARKER = "# EPY: stripped_notebook: "
+EPY_START_MARKER = "# EPY: START "
+EPY_TRANSFORM_MARKER = "# EPY: ESCAPE "
+EPY_END_MARKER = "# EPY: END "
+
+
+def decode(args):
+    notebook = {
+        'cells': []
+    }
+
+    with open(args.source, "r") as fh:
+        iterator = iter(fh.readlines())
+
+        # read until we get the stripped notebook marker.
+        for line in iterator:
+            if line.startswith(STRIPPED_NOTEBOOK_MARKER):
+                remainder = line[len(STRIPPED_NOTEBOOK_MARKER):]
+                notebook.update(json.loads(remainder))
+                break
+
+        while True:
+            cell = {
+                'metadata': {},
+                'source': [],
+            }
+            # read until the first start marker
+            for line in iterator:
+                if line.startswith(EPY_START_MARKER):
+                    remainder = line[len(EPY_START_MARKER):].rstrip()
+                    cell['cell_type'] = remainder
+
+                    expected_transform = EPY_TRANSFORM_MARKER
+                    expected_end_marker = EPY_END_MARKER
+                    break
+            else:
+                break
+
+            for line in iterator:
+                if line.startswith(expected_end_marker):
+                    break
+                if line.startswith(expected_transform):
+                    remainder = line[len(expected_transform):]
+                    cell['source'].append(remainder)
+                    continue
+
+                if cell['cell_type'] == 'markdown':
+                    line = line[2:]
+                cell['source'].append(line)
+
+            if cell['cell_type'] == 'code':
+                cell['execution_count'] = None
+                cell['outputs'] = []
+
+            # strip the final "\n" from the last line.  it's weird.
+            if cell['source'][-1].endswith("\n"):
+                cell['source'][-1] = cell['source'][-1][:-1]
+
+            notebook['cells'].append(cell)
+
+    with open(args.output, "w") as fh:
+        json.dump(
+            notebook, fh,
+            sort_keys=True, indent=1, separators=(',', ': '))
+
+
+def encode(args):
+    encoder = EncodedPythonExporter()
+    encoded, _ = encoder.from_filename(args.notebook)
+    with open(args.output, "w") as fh:
+        fh.write(encoded)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command")
+
+    decode_group = subparsers.add_parser("decode")
+    decode_group.set_defaults(command=decode)
+    decode_group.add_argument("source", help="The python with extra state embedded to reconstitute an .ipynb file")
+    decode_group.add_argument("output", help="The reconstituted .ipynb file")
+
+    encode_group = subparsers.add_parser("encode")
+    encode_group.set_defaults(command=encode)
+    encode_group.add_argument("notebook", help="The .ipynb file to encode to a .py file")
+    encode_group.add_argument("output", help="The encoded .py file")
+
+    return parser, parser.parse_args()
+
+
+def main():
+    parser, args = parse_args()
+
+    if args.command is None:
+        parser.print_help()
+        parser.exit(status=2)
+    args.command(args)
