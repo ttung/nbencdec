f5610c12fc630f6c127164e7de99c204714023ad
Tony Tung <ttung@chanzuckerberg.com>
2018-02-15 23:04:39 -0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import os
+import setuptools
+
+install_requires = [line.rstrip() for line in open(os.path.join(os.path.dirname(__file__), "requirements.txt"))]
+
+setuptools.setup(
+    name="nbencdec",
+    version="0.0.1",
+    description="Encode/decode Python Notebook files to .py files.",
+    author="Tony Tung",
+    author_email="ttung@chanzuckerberg.com",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': "nbencdec=nbencdec:main",
+    }
+)
